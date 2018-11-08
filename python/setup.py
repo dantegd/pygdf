3a2b4b16a67bc0d68d316e284985a9aa947d8d66
Dante Gama Dessavre <galahad@Dantes-MacBook-Pro.local>
2018-11-07 21:05:01 -0600

    full refactor together

---
+++ python/setup.py
@@ -0,0 +1,64 @@
+from setuptools import setup, find_packages
+from setuptools.extension import Extension
+from Cython.Build import cythonize
+import numpy
+
+import os
+import versioneer
+import sys
+from distutils.sysconfig import get_python_lib
+
+
+packages = ['cudf',
+            'cudf.bind',
+            'cudf.tests'
+            ]
+
+install_requires = [
+    'numba',
+    'cython'
+]
+
+try:
+    numpy_include = numpy.get_include()
+except AttributeError:
+    numpy_include = numpy.get_numpy_include()
+
+cython_files = ['cudf/bind/*.pyx']
+
+extensions = [
+    Extension("*",
+              sources=cython_files,
+              include_dirs=[numpy_include, '../cpp/include/'],
+              # library_dirs=['/home/dante/miniconda3/envs/cpp-ref2/lib/'],
+              library_dirs=[get_python_lib()],
+              libraries=['cudf'],
+              language='c++',
+              extra_compile_args=['-std=c++11'],
+              runtime_library_dirs=['/usr/local/lib'])
+]
+
+setup(name='cudf',
+      description="cuDF - GPU Dataframe",
+      version=versioneer.get_version(),
+      classifiers=[
+        # "Development Status :: 4 - Beta",
+        "Intended Audience :: Developers",
+        # "Operating System :: OS Independent",
+        "Programming Language :: Python",
+        # "Programming Language :: Python :: 2.7",
+        "Programming Language :: Python :: 3.5",
+      ],
+      # Include the separately-compiled shared library
+      author="NVIDIA Corporation",
+      setup_requires=['cython'],
+      ext_modules=cythonize(extensions),
+      packages=packages,
+      package_data={
+        'cudf.tests': ['data/*.pickle'],
+      },
+      install_requires=install_requires,
+      license="Apache",
+      cmdclass=versioneer.get_cmdclass(),
+      zip_safe=False
+      )
